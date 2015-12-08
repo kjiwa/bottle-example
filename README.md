5eabd1fc410dea133192a3969b30d6753ed104c8
Kamil Jiwa <kamil.jiwa@gmail.com>
2015-12-07 17:26:02 -0800

    Add a simple server implementation with Heroku config files.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bottle-example
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+A simple Python web server using the Bottle framework.
---
+++ server.py
@@ -0,0 +1,10 @@
+import bottle
+
+APP = bottle.default_app()
+
+@bottle.route('/')
+def index():
+  return '<p>Hello</p>'
+
+if __name__ == '__main__':
+  bottle.run(application=APP)
